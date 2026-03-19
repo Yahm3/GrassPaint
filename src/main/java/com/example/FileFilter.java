8cb2d50eddbfc8787ff9710c5acf77ac92dbd1f4
yahm3 <240418541@tut4life.ac.za>
2026-03-19 02:04:45 +0200

    feat: support other image files

--- src/main/java/com/example/FileFilter.java
+++ src/main/java/com/example/FileFilter.java
@@ -4,5 +4,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileFilter {
   public static final FileNameExtensionFilter png = new FileNameExtensionFilter("PNG files", "png");
-  public static final FileNameExtensionFilter jpg = new FileNameExtensionFilter("PNG files", "jpg");
+  public static final FileNameExtensionFilter jpg = new FileNameExtensionFilter("JPG files", "jpg");
+  public static final FileNameExtensionFilter jpeg = new FileNameExtensionFilter("JPEG files", "jpeg");
+  public static final FileNameExtensionFilter ppm = new FileNameExtensionFilter("PPM files", "ppm");
 }
